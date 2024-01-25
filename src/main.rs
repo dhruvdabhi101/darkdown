42b622d235ecaea15729e9dddabf469d4f97d837
Dhruv <dhruvdabhi101@gmail.com>
2024-01-25 08:46:32 +0530

    feat: using clap to parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,24 @@
-use std::fs::{self, File};
 use crate::converter::converter::Converter;
+use clap::Parser;
+use std::fs::{self, File};
 pub mod converter;
 
+#[derive(Default, Parser, Debug)]
+/// Convert a .dd file to .html
+struct Config {
+    /// Input file name to convert
+    input_file: String,
+}
+
 fn main() {
-    #[allow(unused_variables)]
-    let custom_markup = r#"
-@ Heading 1
-
-**bold**
-
-^Italic^
-
-\Code\
-
-
-\\\\
-Code
-\\\\
-
-~~Text~~Link~~
----
-
-This is syntax of my own markup language.
-"#;
-    let args: Vec<_> = std::env::args().collect();
-    if args.len() > 1 {
-        let file_name = &args[1];
-        let file_content = std::fs::read_to_string(file_name).unwrap();
-        let file_name_without_ext = file_name.split(".").collect::<Vec<&str>>()[0];
-        let ext = file_name.split(".").collect::<Vec<&str>>()[1];
-        if ext != "dd" {
-            panic!("File extension is not supported");
-        }
+    let args = Config::parse();
+    let file_name = &args.input_file;
+    let file_content = std::fs::read_to_string(file_name).unwrap();
+    let file_name_without_ext = file_name.split(".").collect::<Vec<&str>>()[0];
+    let ext = file_name.split(".").collect::<Vec<&str>>()[1];
+    if ext != "dd" {
+        panic!("File extension is not supported");
+    } else {
         let html_file_name = format!("{}.html", file_name_without_ext);
 
         let mut converter = Converter::new();
